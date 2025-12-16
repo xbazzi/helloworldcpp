0a603cf030ff9d79aa84119332c171c0f213a88c
xbazzi <codemaster@xbazzi.com>
2025-12-15 22:05:55 -0700

    feat: add vector print functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
+#include <algorithm>
 #include <iostream>
+#include <vector>
 
-int main(){
-    int x=5;
+int main()
+{
+    int x = 5;
     // std::print("hello world {}", x);
     std::cerr << "helloworld " << x << '\n';
-    
+    std::vector<std::uint64_t> uints;
+    std::for_each(uints.begin(), uints.end(), [](const auto &i) { std::cout << i << " "; });
+
     return 0;
 }
-
-
